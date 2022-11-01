fce3fa8168c60e48d77998195f6a039f7262dc18
nuzcraft <nuzcraft@gmail.com>
2022-11-01 07:58:13 -0500

    added droprates json and integrated it into loot manager

--- fps_boss_crafting/Loot/LootManager.gd
+++ fps_boss_crafting/Loot/LootManager.gd
@@ -1,19 +1,51 @@
 class_name LootManager
 
 const loot = preload("res://Loot/Loot.tscn")
+var rng = RandomNumberGenerator.new()
+
+var droprates_dict = {}
+var monster_drops = {}
+
+var item_dict = {
+	"eye": Item.EYE,
+	"skull": Item.SKULL
+}
 
 func _init():
 	pass
+	
+func read_droprates_file():
+	var file = File.new()
+	file.open("res://Loot/DropRates.json", File.READ)
+	droprates_dict = parse_json(file.get_as_text())
+	
+func get_monster_drops(name):
+	monster_drops = droprates_dict[name]
+	var current_sum = 0
+	for drop in monster_drops:
+		current_sum += monster_drops[drop]
+		monster_drops[drop] = current_sum
 
 func get_loot(name):
-	if name == "zombie":
-		var skull_item = Item.new(Item.SKULL)
-		var loot_inst = loot.instance()
-		loot_inst.set_item(skull_item)
-		var skull_item2 = Item.new(Item.SKULL)
-		var loot_inst2 = loot.instance()
-		loot_inst2.set_item(skull_item2)
-		var eye_item = Item.new(Item.EYE)
-		var loot_inst3 = loot.instance()
-		loot_inst3.set_item(eye_item)
-		return [loot_inst, loot_inst2, loot_inst3]
+	
+	read_droprates_file()
+	get_monster_drops(name)
+	
+	rng.randomize()
+	
+	var loot_array = []
+	
+	for n in 3:
+		var random_float = randf()
+		for drop in monster_drops:
+			if random_float <= monster_drops[drop]:
+				var loot_inst = create_loot_instance(drop)
+				loot_array.append(loot_inst)
+				break
+	return loot_array	
+	
+func create_loot_instance(drop_name):
+	var item = Item.new(item_dict[drop_name])
+	var loot_inst = loot.instance()
+	loot_inst.set_item(item)
+	return loot_inst
